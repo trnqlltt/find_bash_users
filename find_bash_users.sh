3480d0b08137bef4303bd084d59043d6af10ae43
Olena Kozakova <135411100+trnqlltt@users.noreply.github.com>
2025-04-26 13:46:28 +0300

    Update find_bash_users.sh

--- find_bash_users.sh
+++ find_bash_users.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # Script to search for all users with the /bin/bash shell
 
-grep "/bin/bash" /etc/passwd > bash_users.txt
+# Check if the /etc/passwd file is available
+if [[ ! -f /etc/passwd ]]; then
+    echo "File /etc/passwd not found!"
+    exit 1
+fi
+
+# Search for users with /bin/bash shell
+awk -F: '$7 == "/bin/bash" {print $1}' /etc/passwd
+
 
